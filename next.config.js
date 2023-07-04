b346eac938581735254a0a78d3fea2ff86e6053f
Kareen <kjabellar@gmail.com>
2023-07-04 11:05:02 +0800

    added s3 links

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.vnoc.com', 'vnoclogos.s3-us-west-1.amazonaws.com', 'tools.contrib.com','projectcafe.com', 'contrib.com'],
+    domains: ['cdn.vnoc.com', 'vnoclogos.s3-us-west-1.amazonaws.com', 'tools.contrib.com','projectcafe.com', 'contrib.com','vnoclogos.s3-us-west-1.amazonaws.com'],
   },
 }
 
