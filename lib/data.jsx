d1265ff9a7d36be2e062a11bd09152dd3aebf42b
Kareen <kjabellar@gmail.com>
2024-05-30 10:52:23 +0800

    replaced axios to fetch

--- lib/data.jsx
+++ lib/data.jsx
@@ -14,7 +14,7 @@ export function getDomain() {
 export async function getData() {
   const domain = getDomain();
   const url = process.env.CONTRIB_API1+`&domain=${domain}`
-  const res = await fetch(url);
+  const res = await fetch(url, { next: { revalidate: 3600 } });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
@@ -26,7 +26,7 @@ export async function getData() {
 export async function getTopsites() {
   const domain = getDomain();
   const url = process.env.CONTRIB_API1_TOPSITES+`&domain=${domain}`
-  const res = await fetch(url);
+  const res = await fetch(url, { next: { revalidate: 3600 } });
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
